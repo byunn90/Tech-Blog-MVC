ab8409f53b743c10bfc8674a544e8ccc72021ddf
byunn90 <kayhanturk90@Live.com>
2023-03-27 22:21:11 +1100

    test1 again

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const Post = require("./post");
 
-module.exports = { Post };
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Post };
